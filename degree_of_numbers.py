7b0dda65b049d898031f3781524c5cde9b7797a3
Ruslan Nazarov <hlfan2@yandex.ru>
2022-01-31 15:55:09 +0700

    fix labels

--- degree_of_numbers.py
+++ degree_of_numbers.py
@@ -7,9 +7,9 @@ y_values = [x**get_y for x in x_values]
 plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolors=None, s=50)
 
 # Назначение заголовка диаграмы и меток осей.
-plt.title('Square numbers', fontsize=24)
+plt.title('Degree of numbers', fontsize=24)
 plt.xlabel('Value', fontsize=14)
-plt.ylabel('Square of values', fontsize=14)
+plt.ylabel('Degree of values', fontsize=14)
 
 # Назначение шрифта делений на осях.
 plt.tick_params(axis='both', labelsize=14)
