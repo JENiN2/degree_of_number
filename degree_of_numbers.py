e990eeee315e30011a3960739d2073f3f4050f4e
Ruslan Nazarov <hlfan2@yandex.ru>
2022-01-31 15:45:08 +0700

    add input

--- degree_of_numbers.py
+++ degree_of_numbers.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 
-x_values = list(range(1, 6))
-y_values = [x**2 for x in x_values]
-plt.scatter(x_values, y_values, c='orange', edgecolors=None, s=50)
+get_x = int(input(f'Enter the length of the sequence: ')) + 1
+get_y = int(input(f'Enter the degree: '))
+x_values = list(range(1, get_x))
+y_values = [x**get_y for x in x_values]
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolors=None, s=50)
 
 # Назначение заголовка диаграмы и меток осей.
 plt.title('Square numbers', fontsize=24)
@@ -11,8 +13,5 @@ plt.ylabel('Square of values', fontsize=14)
 
 # Назначение шрифта делений на осях.
 plt.tick_params(axis='both', labelsize=14)
-
-# Назначение диапазона для каждой оси.
-plt.axis([0, 6, 0, 60])
 
 plt.show()
