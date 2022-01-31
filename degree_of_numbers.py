9e2ff058af174c52a8a87afef777adc964f84168
Ruslan Nazarov <hlfan2@yandex.ru>
2022-01-31 15:08:37 +0700

    automation of calculations

--- degree_of_numbers.py
+++ degree_of_numbers.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
-plt.scatter(x_values, y_values, s=50)
+x_values = list(range(1, 6))
+y_values = [x**2 for x in x_values]
+plt.scatter(x_values, y_values, c='orange', edgecolors=None, s=50)
 
 # Назначение заголовка диаграмы и меток осей.
 plt.title('Square numbers', fontsize=24)
@@ -11,5 +11,8 @@ plt.ylabel('Square of values', fontsize=14)
 
 # Назначение шрифта делений на осях.
 plt.tick_params(axis='both', labelsize=14)
+
+# Назначение диапазона для каждой оси.
+plt.axis([0, 6, 0, 60])
 
 plt.show()
